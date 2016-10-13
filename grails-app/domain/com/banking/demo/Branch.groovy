cebc4460877595f75db45890eb3d9cfa1ac82232
Gregg Bolinger <hycel1@gmail.com>
2016-10-13 10:23:52 -0700

    new branch domain

---
+++ grails-app/domain/com/banking/demo/Branch.groovy
@@ -0,0 +1,28 @@
+package com.banking.demo
+
+class Branch {
+  Long externalId
+  String name
+  String address
+  String city
+  String state
+  String zipCode
+  String country
+  String phoneNumber
+
+//  static hasMany = [
+//    accounts : Account
+//  ]
+
+  static constraints = {
+    externalId nullable: false, unique: true
+    name nullable: false, blank: false, maxSize: 40
+    address nullable: false, blank: false, maxSize: 50
+    city nullable: false, blank: false, maxSize: 25
+    state nullable: false, blank: false, maxSize: 25
+    zipCode nullable: false, blank: false, maxSize: 10
+    country nullable: false, blank: false, maxSize: 25
+    phoneNumber nullable: false, blank: false, maxSize: 25
+  }
+
+}
