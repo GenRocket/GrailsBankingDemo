67e80963a46958191770358e2812adfffbb71ca6
Hycel Taylor <hycel1@gmail.com>
2016-10-18 01:59:35 -0700

    formatting

--- grails-app/controllers/com/genrocket/bank/AccountController.groovy
+++ grails-app/controllers/com/genrocket/bank/AccountController.groovy
@@ -5,11 +5,13 @@ class AccountController {
 
   def withdrawal() {
     Card card = bankingService.selectedCard
+
     if (!card) {
       redirect(controller: 'home', action: 'index')
     } else {
       card = Card.get(card.id)    // To fix : could not initialize proxy - no Session
       AccountType accountType = card.customer?.account?.accountType
+
       if (accountType.name == AccountTypes.CHECKING.getValue()) {
         render(view: 'withdrawal')
       } else {
