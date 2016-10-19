c8e6c33f80c062adca24528af2192beeca06da5f
Hycel Taylor <hycel1@gmail.com>
2016-10-19 01:29:59 -0700

    GR-1573 Create BankDemo checkingService.withdrawal() GR-1574 Create White Box Tests for BankDemo checking.withdrawal()

--- test/integration/com/genrocket/bank/CheckingServiceIntegrationSpec.groovy
+++ test/integration/com/genrocket/bank/CheckingServiceIntegrationSpec.groovy
@@ -2,10 +2,148 @@ package com.genrocket.bank
 
 import grails.test.spock.IntegrationSpec
 
+import java.text.SimpleDateFormat
+
 class CheckingServiceIntegrationSpec extends IntegrationSpec {
   def checkingService
   def accountTestDataService
   def transactionTestDataService
+  def transactionTypeTestDataService
+
+  void "test withdrawal ACCOUNT_NOT_CHECKING"() {
+    given:
+
+    accountTestDataService.loadData(1)
+
+    Account account = Account.first()
+    Customer customer = Customer.findByAccount(account)
+    User user = customer.user
+    CustomerLevel customerLevel = customer.customerLevel
+
+    AccountType accountType = AccountType.findByName(AccountTypes.SAVINGS.value)
+
+    when:
+
+    account.balance = 100.0
+    account.accountType = accountType
+    account.save()
+
+    customerLevel.overdraftAllowed = true
+    customerLevel.dailyWithdrawalLimit = 100
+    customerLevel.save()
+
+    TransactionStatus status = checkingService.withdrawal(user, account, 100)
+
+    then:
+
+    status == TransactionStatus.ACCOUNT_NOT_CHECKING
+  }
+
+  void "test withdrawal OVERDRAFT_NOT_ALLOWED"() {
+    given:
+
+    accountTestDataService.loadData(1)
+
+    Account account = Account.first()
+    Customer customer = Customer.findByAccount(account)
+    User user = customer.user
+    CustomerLevel customerLevel = customer.customerLevel
+
+    AccountType accountType = AccountType.findByName(AccountTypes.CHECKING.value)
+
+    when:
+
+    account.balance = 100.00
+    account.accountType = accountType
+    account.save()
+
+    customerLevel.overdraftAllowed = false
+    customerLevel.dailyWithdrawalLimit = 100
+    customerLevel.save()
+
+    TransactionStatus status = checkingService.withdrawal(user, account, 101)
+
+    then:
+
+    status == TransactionStatus.OVERDRAFT_NOT_ALLOWED
+  }
+
+  void "test withdrawal WITHDRAWAL_LIMIT_REACHED"() {
+    given:
+
+    accountTestDataService.loadData(1)
+
+    Account account = Account.first()
+    Customer customer = Customer.findByAccount(account)
+    User user = customer.user
+    CustomerLevel customerLevel = customer.customerLevel
+
+    AccountType accountType = AccountType.findByName(AccountTypes.CHECKING.value)
+
+    when:
+
+    account.balance = 500.0
+    account.accountType = accountType
+    account.save()
+
+    customerLevel.overdraftAllowed = false
+    customerLevel.dailyWithdrawalLimit = 150
+    customerLevel.dailyWithdrawalLimit = 100
+    customerLevel.save()
+
+    checkingService.withdrawal(user, account, 250)
+
+    TransactionStatus status = checkingService.withdrawal(user, account, 250)
+
+    then:
+
+    status == TransactionStatus.WITHDRAWAL_LIMIT_REACHED
+  }
+
+  void "test withdrawal TRANSACTION_COMPLETE"() {
+    given:
+
+    accountTestDataService.loadData(1)
+    transactionTypeTestDataService.loadData()
+
+    Account account = Account.first()
+    Customer customer = Customer.findByAccount(account)
+    User user = customer.user
+    CustomerLevel customerLevel = customer.customerLevel
+
+    AccountType accountType = AccountType.findByName(AccountTypes.CHECKING.value)
+    TransactionType transactionType = TransactionType.findByName(TransactionTypes.WITHDRAWAL_CHECKING.value)
+
+    Float balance = 500.00
+    Float withdrawalAmount = 250
+
+    SimpleDateFormat sdf = new SimpleDateFormat('yyyy/MM/dd')
+
+    when:
+
+    account.balance = balance
+    account.accountType = accountType
+    account.save()
+
+    customerLevel.overdraftAllowed = false
+    customerLevel.dailyWithdrawalLimit = 1000
+    customerLevel.save()
+
+    checkingService.withdrawal(user, account, withdrawalAmount)
+
+    TransactionStatus status = checkingService.withdrawal(user, account, withdrawalAmount)
+    Transaction transaction = Transaction.findByUserAndAccount(user, account)
+    String dateCreated = sdf.parse(sdf.format(transaction.dateCreated))
+    String today = sdf.parse(sdf.format(new Date()))
+
+    then:
+
+    status == TransactionStatus.TRANSACTION_COMPLETE
+    transaction != null
+    transaction.transactionType == transactionType
+    transaction.amount.round(2) == ((Float) balance - withdrawalAmount).round(2)
+    dateCreated == today
+  }
 
   void "test check daily withdrawal limit reached"() {
     given:
