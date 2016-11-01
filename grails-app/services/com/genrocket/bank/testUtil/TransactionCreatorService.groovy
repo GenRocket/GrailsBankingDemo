247d60cbc886f9d579ca998eb097ccc1ad3f8955
Gregg Bolinger <hycel1@gmail.com>
2016-11-01 22:29:43 +0530

    optimized

--- grails-app/services/com/genrocket/bank/testUtil/TransactionCreatorService.groovy
+++ grails-app/services/com/genrocket/bank/testUtil/TransactionCreatorService.groovy
@@ -13,6 +13,7 @@ import grails.transaction.Transactional
 
 @Transactional
 class TransactionCreatorService {
+  def cardService
   def accountService
   def userTestDataService
   def branchTestDataService
@@ -22,7 +23,8 @@ class TransactionCreatorService {
   def customerLevelTestDataService
   def transactionTypeTestDataService
 
-  Map createCheckingAndSavingsAccount() {
+  void createCheckingAndSavingsAccount(Integer count) {
+    userTestDataService.loadData(count)
     cardPoolTestDataService.loadData(100)
     transactionTypeTestDataService.loadData()
 
@@ -33,17 +35,46 @@ class TransactionCreatorService {
     branchTestDataService.loadData()
     Branch branch = Branch.first()
 
-    userTestDataService.loadData()
-    User user = User.first()
-
     cardTypeTestDataService.loadData()
     CardType cardType = CardType.first()
 
     customerLevelTestDataService.loadData()
     CustomerLevel customerLevel = CustomerLevel.first()
 
-    Account checkingAccount = accountService.save(user, branch, cardType, checkingType, customerLevel)
-    Account savingsAccount = accountService.save(user, branch, cardType, savingsType, customerLevel)
+    List<User> users = User.findAll()
+
+    users.each { user ->
+      println "Creating accounts for User ${user.id}. ${user.firstName} ${user.lastName}"
+
+      accountService.save(user, branch, cardType, checkingType, customerLevel)
+      accountService.save(user, branch, cardType, savingsType, customerLevel)
+
+      List<Customer> customers = Customer.findAllByUser(user) as Customer[]
+
+      customers.each { customer ->
+        customer.enabled = true
+        customer.save()
+
+        customer.customerLevel.dailyWithdrawalLimit = 500
+        customer.customerLevel.monthlyMaxTransfersAllowed = 3
+        customer.customerLevel.overdraftAllowed = true
+        customer.customerLevel.save()
+
+        Card card = Card.findByCustomer(customer)
+        cardService.activateCard(card, 123456)
+      }
+    }
+  }
+
+  Map createCheckingAndSavingsAccount() {
+    createCheckingAndSavingsAccount(1)
+
+    AccountType checkingType = AccountType.findByName(AccountTypes.CHECKING.value)
+    AccountType savingsType = AccountType.findByName(AccountTypes.SAVINGS.value)
+    User user = User.first()
+
+    Account checkingAccount = Account.findByAccountType(checkingType)
+    Account savingsAccount = Account.findByAccountType(savingsType)
 
     Customer checkingCustomer = Customer.findByUserAndAccount(user, checkingAccount)
     Customer savingsCustomer = Customer.findByUserAndAccount(user, savingsAccount)
@@ -51,24 +82,8 @@ class TransactionCreatorService {
     CustomerLevel checkingCustomerLevel = checkingCustomer.customerLevel
     CustomerLevel savingsCustomerLevel = savingsCustomer.customerLevel
 
-    checkingCustomer.enabled = true
-    checkingCustomer.save()
-
-    savingsCustomer.enabled = true
-    savingsCustomer.save()
-
     Card checkingCard = Card.findByCustomer(checkingCustomer)
     Card savingsCard = Card.findByCustomer(savingsCustomer)
-
-    checkingCustomerLevel.dailyWithdrawalLimit = 500
-    checkingCustomerLevel.monthlyMaxTransfersAllowed = 3
-    checkingCustomerLevel.overdraftAllowed = true
-    checkingCustomerLevel.save()
-
-    savingsCustomerLevel.dailyWithdrawalLimit = 500
-    savingsCustomerLevel.monthlyMaxTransfersAllowed = 3
-    savingsCustomerLevel.overdraftAllowed = true
-    savingsCustomerLevel.save()
 
     return [
       user: user,
