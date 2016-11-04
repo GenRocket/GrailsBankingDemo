74ab9547e8320fe36ec3c520eaddde00e4930adb
Gregg Bolinger <hycel1@gmail.com>
2016-11-04 06:17:44 +0530

    Only load data when it is needed

--- grails-app/services/com/genrocket/bank/BootstrapService.groovy
+++ grails-app/services/com/genrocket/bank/BootstrapService.groovy
@@ -18,37 +18,39 @@ class BootstrapService {
     CustomerLevel[] customerLevels = CustomerLevel.findAll() as CustomerLevel[]
     Integer customerLevelIndex = 0
 
-    users.each { user ->
-      println "Creating accounts for User ${user.id}. ${user.firstName} ${user.lastName}"
-      CustomerLevel customerLevel = customerLevels[customerLevelIndex]
-      Account account1 = accountService.save(user, branch, cardType, checking, customerLevel)
-      Account account2 = accountService.save(user, branch, cardType, savings, customerLevel)
+    if (User.count() == 0) {
+      users.each { user ->
+        println "Creating accounts for User ${user.id}. ${user.firstName} ${user.lastName}"
+        CustomerLevel customerLevel = customerLevels[customerLevelIndex]
+        Account account1 = accountService.save(user, branch, cardType, checking, customerLevel)
+        Account account2 = accountService.save(user, branch, cardType, savings, customerLevel)
 
-      account1.accountNumber = accountNumber++
-      account2.accountNumber = accountNumber++
+        account1.accountNumber = accountNumber++
+        account2.accountNumber = accountNumber++
 
-      account1.save()
-      account2.save()
+        account1.save()
+        account2.save()
 
-      if (++customerLevelIndex == 3) {
-        customerLevelIndex = 0
+        if (++customerLevelIndex == 3) {
+          customerLevelIndex = 0
+        }
       }
-    }
 
-    User user = User.first()
-    List<Customer> customers = Customer.findAllByUser(user) as Customer[]
+      User user = User.first()
+      List<Customer> customers = Customer.findAllByUser(user) as Customer[]
 
-    customers.each { customer ->
-      customer.enabled = true
-      customer.save()
+      customers.each { customer ->
+        customer.enabled = true
+        customer.save()
 
-      customer.customerLevel.dailyWithdrawalLimit = 500
-      customer.customerLevel.monthlyMaxTransfersAllowed = 3
-      customer.customerLevel.overdraftAllowed = true
-      customer.customerLevel.save()
+        customer.customerLevel.dailyWithdrawalLimit = 500
+        customer.customerLevel.monthlyMaxTransfersAllowed = 3
+        customer.customerLevel.overdraftAllowed = true
+        customer.customerLevel.save()
 
-      Card card = Card.findByCustomer(customer)
-      cardService.activateCard(card, 123456)
+        Card card = Card.findByCustomer(customer)
+        cardService.activateCard(card, 123456)
+      }
     }
   }
 }
