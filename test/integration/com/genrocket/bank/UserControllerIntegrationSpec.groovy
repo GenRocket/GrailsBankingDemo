419a1934b1bebf55887a5fa3f1e813731c75bc36
htaylor <hycel1@gmail.com>
2016-11-30 16:57:56 +0530

    Added test cases for accounts action of UserController

--- test/integration/com/genrocket/bank/UserControllerIntegrationSpec.groovy
+++ test/integration/com/genrocket/bank/UserControllerIntegrationSpec.groovy
@@ -9,111 +9,138 @@ import grails.test.spock.IntegrationSpec
  */
 class UserControllerIntegrationSpec extends IntegrationSpec {
 
-    def userTestDataService
-    def messageSource
+  def userTestDataService
+  def customerTestDataService
+  def messageSource
 
-    void "test User list"() {
-        given:
-        userTestDataService.loadData(20)
+  void "test User list"() {
+    given:
+    userTestDataService.loadData(20)
 
-        when:
-        UserController controller = new UserController()
-        Map map = controller.list()
+    when:
+    UserController controller = new UserController()
+    Map map = controller.list()
 
-        then:
-        map.count == User.count()
-        map.users == User.list([max: 15, sort: 'id', order: 'desc'])
-    }
+    then:
+    map.count == User.count()
+    map.users == User.list([max: 15, sort: 'id', order: 'desc'])
+  }
 
-    void "test User list with pagination"() {
-        given:
-        userTestDataService.loadData(10)
+  void "test User list with pagination"() {
+    given:
+    userTestDataService.loadData(10)
 
-        when:
-        UserController controller = new UserController()
-        controller.params.max = 5
-        controller.params.offset = 5
-        Map map = controller.list()
+    when:
+    UserController controller = new UserController()
+    controller.params.max = 5
+    controller.params.offset = 5
+    Map map = controller.list()
 
-        then:
-        map.count == User.count()
-        map.users == User.list([max: 5, offset: 5, sort: 'id', order: 'desc'])
+    then:
+    map.count == User.count()
+    map.users == User.list([max: 5, offset: 5, sort: 'id', order: 'desc'])
 
-    }
+  }
 
-    void "test User edit"() {
-        given:
-        userTestDataService.loadData()
-        User user = User.first()
+  void "test User edit"() {
+    given:
+    userTestDataService.loadData()
+    User user = User.first()
 
-        when:
-        UserController controller = new UserController()
-        Map map = controller.edit(user.id)
+    when:
+    UserController controller = new UserController()
+    Map map = controller.edit(user.id)
 
-        then:
-        map.user == user
-        map.user.id
-    }
+    then:
+    map.user == user
+    map.user.id
+  }
 
-    void "test User add"() {
-        given:
+  void "test User add"() {
+    given:
 
-        when:
-        UserController controller = new UserController()
-        Map map = controller.edit(null)
+    when:
+    UserController controller = new UserController()
+    Map map = controller.edit(null)
 
-        then:
-        map.user != null
-        !map.user.id
-    }
+    then:
+    map.user != null
+    !map.user.id
+  }
 
-    void "test User save for newly added"() {
-        given:
-        List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
-        User user = (User) userList.first().object
+  void "test User save for newly added"() {
+    given:
+    List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
+    User user = (User) userList.first().object
 
-        when:
-        UserController controller = new UserController()
-        controller.save(user)
+    when:
+    UserController controller = new UserController()
+    controller.save(user)
 
-        then:
-        user.id
-        User.count()
-        controller.flash.message == messageSource.getMessage("user.successfully.added", null, null)
-        controller.response.redirectedUrl == '/user/list'
-    }
+    then:
+    user.id
+    User.count()
+    controller.flash.message == messageSource.getMessage("user.successfully.added", null, null)
+    controller.response.redirectedUrl == '/user/list'
+  }
 
-    void "test User save for edited"() {
-        given:
-        List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
-        User user = (User) userList.first().object
-        user.save(flush: true)
+  void "test User save for edited"() {
+    given:
+    List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
+    User user = (User) userList.first().object
+    user.save(flush: true)
 
-        when:
-        UserController controller = new UserController()
-        controller.save(user)
+    when:
+    UserController controller = new UserController()
+    controller.save(user)
 
-        then:
-        user.id
-        User.count()
-        controller.flash.message == messageSource.getMessage("user.successfully.edited", null, null)
-        controller.response.redirectedUrl == '/user/list'
-    }
+    then:
+    user.id
+    User.count()
+    controller.flash.message == messageSource.getMessage("user.successfully.edited", null, null)
+    controller.response.redirectedUrl == '/user/list'
+  }
 
-    void "test validation error on User save"() {
-        given:
-        List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
-        User user = (User) userList.first().object
-        user.firstName = null
+  void "test validation error on User save"() {
+    given:
+    List<LoaderDTO> userList = (List<LoaderDTO>) UserTestDataLoader.load()
+    User user = (User) userList.first().object
+    user.firstName = null
 
-        when:
-        UserController controller = new UserController()
-        controller.save(user)
+    when:
+    UserController controller = new UserController()
+    controller.save(user)
 
-        then:
-        !user.id
-        !User.count()
-        user.errors.getFieldError("firstName").code == "nullable"
-        controller.modelAndView.viewName == '/user/edit'
-    }
+    then:
+    !user.id
+    !User.count()
+    user.errors.getFieldError("firstName").code == "nullable"
+    controller.modelAndView.viewName == '/user/edit'
+  }
+
+  void "test User accounts for null Id"() {
+    given:
+
+    when:
+    UserController controller = new UserController()
+    Map map = controller.accounts(null)
+
+    then:
+    map.user == null
+    map.customers == []
+  }
+
+  void "test User accounts"() {
+    given:
+    customerTestDataService.loadData(1)
+    User user = User.first()
+
+    when:
+    UserController controller = new UserController()
+    Map map = controller.accounts(user.id)
+
+    then:
+    map.user == user
+    map.customers == Customer.findAllByUser(user)
+  }
 }
