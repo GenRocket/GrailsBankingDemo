248645f9924b9e43a2526ec1c346eff0116dd6c5
Hycel Taylor <hycel1@gmail.com>
2017-04-01 18:15:40 -0700

    Updates

--- grails-app/controllers/com/genrocket/bank/RestController.groovy
+++ grails-app/controllers/com/genrocket/bank/RestController.groovy
@@ -4,12 +4,75 @@ import grails.converters.JSON
 
 class RestController {
   def branchService
+  def cardTypeService
+  def cardPoolService
+  def accountTypeService
+  def customerLevelService
+  def transactionTypeService
+
+  def createAccountType() {
+    Map accountTypeMap = request.JSON as Map
+    AccountType accountType = new AccountType()
+    accountType.properties = accountTypeMap.accountType
+
+    accountTypeService.save(accountType)
+
+    render ([success: true] as JSON)
+  }
+
+  def createCardPool() {
+    Map cardPoolMap = request.JSON as Map
+    CardPool cardPool = new CardPool()
+    cardPool.properties = cardPoolMap.cardPool
+
+    cardPoolService.save(cardPool)
+
+    render ([success: true] as JSON)
+  }
+
+  def createCardType() {
+    Map cardTypeMap = request.JSON as Map
+    CardType cardType = new CardType()
+    cardType.properties = cardTypeMap.cardType
+
+    cardTypeService.save(cardType)
+
+    render ([success: true] as JSON)
+  }
+
+  def createCustomerLevel() {
+    Map customerLevelMap = request.JSON as Map
+    CustomerLevel customerLevel = new CustomerLevel()
+    customerLevel.properties = customerLevelMap.customerLevel
+
+    customerLevelService.save(customerLevel)
+
+    render ([success: true] as JSON)
+  }
+
+  def createTransactionType() {
+    Map transactionTypeMap = request.JSON as Map
+    TransactionType transactionType = new TransactionType()
+    transactionType.properties = transactionTypeMap.transactionType
+
+    transactionTypeService.save(transactionType)
+
+    render ([success: true] as JSON)
+  }
 
   def createBranch() {
     Map branchMap = request.JSON as Map
     Branch branch = new Branch()
     branch.properties = branchMap.branch
+
     branchService.save(branch)
+
+    render ([success: true] as JSON)
+  }
+
+  def openAccountWithDeposit() {
+
+
     render ([success: true] as JSON)
   }
 }
