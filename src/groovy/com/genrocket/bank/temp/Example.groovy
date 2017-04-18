1f307299592861d4b87a8bfd7e9de57c3780244f
Hycel Taylor <hycel1@gmail.com>
2017-04-18 11:38:33 -0700

    Soap Example

--- src/groovy/com/genrocket/bank/temp/Example.groovy
+++ src/groovy/com/genrocket/bank/temp/Example.groovy
@@ -10,19 +10,20 @@ class Example {
   public static void main(String[] args) {
 
   def client = new SOAPClient("http://localhost:8080/GrailsBankingDemo/services/bank")
-  def response = client.send(SOAPAction: "createAccountType",
-    connectTimeout: 5000,
+  def response = client.send(connectTimeout: 5000,
     readTimeout: 10000,
     useCaches: false,
     followRedirects: false,
     sslTrustAllCerts: true,
     """<xs:Envelope xmlns:xs='http://schemas.xmlsoap.org/soap/envelope/'>
-        <xs:Body>
-        <name>Checking</name>
-      </xs:Body>
-    </xs:Envelope>
-    """)
+         <xs:Body>
+           <ns1:createAccountType xmlns:ns1='http://bank.genrocket.com/'>
+             <name>Foo</name>
+           </ns1:createAccountType>
+         </xs:Body>
+       </xs:Envelope>"""
+  )
 
-    println response
+    println response.text
   }
 }
