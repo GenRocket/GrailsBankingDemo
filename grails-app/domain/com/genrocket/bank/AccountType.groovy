d93e9e3fcfc574e86c2fba33192f3666ca4aa25a
htaylor <hycel1@gmail.com>
2017-04-18 21:23:52 +0530

    SOAP Changes!

--- grails-app/domain/com/genrocket/bank/AccountType.groovy
+++ grails-app/domain/com/genrocket/bank/AccountType.groovy
@@ -1,6 +1,13 @@
 package com.genrocket.bank
 
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlElement
+
+@XmlAccessorType(XmlAccessType.NONE)
 class AccountType {
+
+  @XmlElement
   String name
 
   static constraints = {
